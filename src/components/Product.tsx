a01ce925647cdd77060551135504592df1f2e4ed
Erdem AY <eirdemmoon@gmail.com>
2023-09-19 17:26:32 +0300

    product card

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ProductType } from "@/utils/types";
 import Link from "next/link";
+
 interface ProductProps {
   product: ProductType;
   location: string;
@@ -8,9 +9,26 @@ interface ProductProps {
 
 const Product: React.FC<ProductProps> = ({ product, location }) => {
   return (
-    <div className="border p-2 w-60">
+    <div className="bg-white rounded-lg border p-2 w-80 h-60 flex items-center justify-between mb-4">
       <Link href={`/product/${product.id}`}>
-        <h2>{product.title}</h2>
+        <div className="flex">
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="w-20 h-20 object-cover mr-4"
+          />
+          <div>
+            <h2 className="text-lg font-semibold">{product.title}</h2>
+            <div className="flex items-center">
+              <span className="text-gray-800 font-semibold">
+                ${product.price}
+              </span>
+              <span className="text-green-500 font-semibold ml-2">
+                {product.discountPercentage}% OFF
+              </span>
+            </div>
+          </div>
+        </div>
       </Link>
     </div>
   );
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,20 +1,19 @@
 import { ProductType } from "@/utils/types";
 import React from "react";
 import Product from "./Product";
+
 interface ProductsProps {
   products: ProductType[];
 }
 
-const Products: React.FC< ProductsProps> = ({ products }) => {
+const Products: React.FC<ProductsProps> = ({ products }) => {
   return (
-    <div className="flex gap-2 flex-wrap">
+    <div className="bg-gradient-to-r from-orange-300 via-orange-600 to-orange-100 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
       {products?.map((item) => (
-        <Product product={item} location="Products" />
+        <Product key={item.id} product={item} location="Products" />
       ))}
     </div>
   );
 };
 
 export default Products;
-
-
